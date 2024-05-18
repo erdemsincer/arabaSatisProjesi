d330c6a523d3f0951b589195fe8c9d29888c9799
Engin Saçan <mastafasgas@gmail.com>
2024-05-18 23:00:33 +0300

    Bazı sekme adları ve ikonları düzeltildi.Ayrıca ana sayfadaki ilan ekle butonu çalışır duruma getirildi.

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,11 +5,14 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>Şifre Yenileme</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+
+        <link rel="icon" type="image/x-icon" href="{{asset("mainpage/assets/car-solid.png")}}"/>
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
--- resources/views/pages/mainpage.blade.php
+++ resources/views/pages/mainpage.blade.php
@@ -40,7 +40,7 @@
                 <nav class="-mx-3 flex flex-1 justify-end">
                     @auth
                         <a
-                            href="#"
+                            href="{{route('sale')}}"
                             class="rounded-md px-3 py-2 ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
                         >
                             İlan Ekle
